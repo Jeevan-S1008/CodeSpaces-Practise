ad8ad99fe30a5ef18c31bf4c3fcddf04412b6c56
Jeevan-S1008 <118654674+Jeevan-S1008@users.noreply.github.com>
2023-06-08 06:57:13 +0000

    Level up

--- TwoOfThree.py
+++ TwoOfThree.py
@@ -4,16 +4,13 @@ m1 = int (input("Enter the marks in the first test: "))
 m2 = int (input("Enter the marks in second test: "))
 m3 = int (input("Enter the marks in third test: "))
 
-if (m1 > m2):
-
-    if (m2 > m3):
-        total = m1 + m2
-    else: 
-        total = m1 + m3
-elif (m1 > m3):
+if (m1 > m3 and m2 > m3):
+    print("greatest marks are ",m1,m2)
     total = m1 + m2
+elif (m1 > m2 and m3 > m2):
+    print("greatest marks are ",m1,m3)
+    total = m1 + m3
 else:
+    print("greatest marks are ",m3,m2)
     total = m2 + m3
-    
-Avg = total / 2
-print ("The average of the best two test marks is: ",Avg)
+print ("The average of the best two test marks is: ",total/2)
